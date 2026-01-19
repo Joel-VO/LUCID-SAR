38295fbdce3ff426e5baf44075472a283c54d5fe
joeloommen <joelvo9694@gmail.com>
2026-01-19 13:20:47 +0530

    update code for venv creation

---
+++ HPC_venv.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Running HPC worker"
+
+echo ""
+
+VENV_DIR="LUCID"
+
+if [ -d "$VENV_DIR" ]; then
+    echo "Virtual environment '$VENV_DIR' exists... Activating..."
+    source "$VENV_DIR/bin/activate"
+else
+    echo "Virtual environment '$VENV_DIR' does not exist. Creating new venv..."
+    if ! python3.11 -m venv "$VENV_DIR"; then
+        echo "Error: Failed to create the virtual environment '$VENV_DIR'." >&2
+        exit 1
+    fi
+    echo "Virtual environment '$VENV_DIR' created successfully. Activating..."
+    source "$VENV_DIR/bin/activate"
+    echo ""
+    echo "Installing venv packages"
+    echo ""
+    pip install --upgrade pip -q
+    pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130 -q
+    pip install -r requirements.txt -q
+    echo ""
+    echo "Installation complete"
+fi
+
+mkdir -p SAR/models
+mkdir -p Logs
+
+echo "complete"
