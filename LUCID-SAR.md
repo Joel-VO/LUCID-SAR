5da73ba305488e8387ff1708b4ac68aa0d0da467
joeloommen <joelvo9694@gmail.com>
2026-03-06 23:30:45 +0530

    euclidean loss update

---
+++ LUCID-SAR.md
@@ -0,0 +1,11 @@
+# Literature references
+---
+
+* asd
+* sdas
+# Procedure
+---
+## Iteration 1
+
+## Iteration 2
+
--- SAR/Denoiser/helpers.py
+++ SAR/Denoiser/helpers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from pytorch_msssim import ssim
 
 
 def euclidean_TV_loss(y_pred, y_ground, lambda_tv = 0.002):
@@ -13,3 +14,22 @@ def euclidean_TV_loss(y_pred, y_ground, lambda_tv = 0.002):
 
     loss = euclidean + lambda_tv*tv_loss
     return loss
+
+
+def euclidean_TV_SSIM_loss(y_pred, y_ground, lambda_tv=0.01, lambda_ssim=0.1):
+
+    l1 = nn.L1Loss()(y_pred, y_ground)
+
+    # TV loss
+    dx = y_pred[:, :, :, 1:] - y_pred[:, :, :, :-1]
+    dy = y_pred[:, :, 1:, :] - y_pred[:, :, :-1, :]
+    tv = torch.mean(torch.abs(dx)) + torch.mean(torch.abs(dy))
+
+
+    ssim_loss = 1 - ssim(y_pred, y_ground, data_range=1.0, size_average=True)
+
+    TV_loss = (lambda_tv * tv)
+    SSIM_loss = (lambda_ssim * ssim_loss)
+    total_loss = l1 + TV_loss+SSIM_loss
+
+    return total_loss
